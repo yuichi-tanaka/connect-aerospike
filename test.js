2273377c352cb9f7d605df2493f5921d18fa1dd0
tanaka-yuichi <tanaka-yuichi@dmm.com>
2014-09-18 12:38:23 +0900

    move test File , mod test command , mot connect-aerospike

--- test.js
+++
@@ -1,7 +0,0 @@
-var session = require('express-session');
-var AerospikeStore = require('./')(session);
-
-var store = new AerospikeStore();
-store.on('connect',function(){
-  console.log('connect');
-});
---
+++ test/test.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+var session = require('express-session');
+var AerospikeStore = require('../')(session);
+
+
+store = new AerospikeStore();
+store.set('abc',{cookie:{maxAge:2000},name:'y.p'},function(err,ok){
+  assert.ok(err.code===0,'set error!!!!');
+  assert.ok(ok.ns==='session','options namespace error!!!!');
+  assert.ok(ok.set ==='store','options session error!!!!');
+  store.get('abc',function(err,ok){
+    console.dir(ok);
+    assert.ok(err.code===0,'get error!!!!');
+    assert.ok(ok.cookie.maxAge===2000,'get maxAge error!!!!');
+    assert.ok(ok.name ==='y.p','get name error!!!!');
+  });
+});
